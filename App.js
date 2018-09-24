5cfd8927eaa4b6d3906211b5dc9157ae6a870255
Haneul Lee <lovesky4294@gmail.com>
2018-09-24 16:31:14 +0900

    weather app finish

--- App.js
+++ App.js
@@ -8,33 +8,51 @@ import {
 } from "react-native";
 import Weather from "./Weather";
 
+const API_KEY = "63c3e210062f9fb0bcfbbc75395d33d9";
+
 export default class App extends Component {
   state = {
-    isLoaded: true
+    isLoaded: true,
+    error: null,
+    temperature: null,
+    name: null
   };
-
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
-        console.log("error");
+        this.setState({
+          error: error
+        });
       }
     );
   }
+  _getWeather = (lat, long) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         {isLoaded ? (
-          <Weather />
+          <Weather weatherName={name} temp={Math.ceil(temperature - 273.15)} />
         ) : (
           <View style={styles.loading}>
             <Text style={styles.loadText}>Getting the weather app!</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -52,6 +70,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#FDF6AA",
     justifyContent: "flex-end",
     paddingLeft: 25
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loadText: {
     fontSize: 38,
